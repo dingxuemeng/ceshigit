d5429886c44774506dca7d7d7d3731d7c697d523
dingxuemeng@weizaojiao.cn <dingxuemeng@weizaojiao.cn>
2020-09-03 10:55:15 +0800

    更新

--- a.php
+++ a.php
@@ -91,7 +91,7 @@ class JwClassController extends IndexController
      *  $condition 排课表查询条件
      *  $where 班级表查询条件
      */
-    public function aa(){
+    public function abc(){
         
     }
 
